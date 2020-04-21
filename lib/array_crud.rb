b42e7d27a14efbe87515ff7ea5eb95cf750aa044
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-21 12:49:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [red, blue, yellow, green]
+  array = ["red", "blue", "yellow", "green"]
 end
 
 def add_element_to_end_of_array(array, element)
